8a62228714e694fb40784d7236163576344cb1cc
HELENTRISHA <helen-trisha_student2021@wilder.school>
2021-07-09 09:48:49 +0200

    mvc structure

---
+++ db/data.js
@@ -0,0 +1,63 @@
+const transactions = [
+    {
+      id: 1,
+      spendings: [
+        {
+          label: "Travels",
+          value: 353.32,
+        },
+        {
+          label: "Groceries",
+          value: 412.45,
+        },
+  
+        {
+          label: "Insurance",
+          value: 202.32,
+        },
+        {
+          label: "Home",
+          value: 202.32,
+        },
+        {
+          label: "Rent",
+          value: 800.55,
+        },
+        {
+          label: "Kids",
+          value: 602.34,
+        },
+      ],
+    },
+    {
+      id: 2,
+      spendings: [
+        {
+          label: "ATravels",
+          value: 10353.32,
+        },
+        {
+          label: "AGroceries",
+          value: 10412.45,
+        },
+  
+        {
+          label: "AInsurance",
+          value: 10202.32,
+        },
+        {
+          label: "AHome",
+          value: 10202.32,
+        },
+        {
+          label: "ARent",
+          value: 10800.55,
+        },
+        {
+          label: "AKids",
+          value: 10602.34,
+        },
+      ],
+    },
+  ];
+  module.exports = transactions;

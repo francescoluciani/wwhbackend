7392e770ffd706386a7150917e53d2039af9244a
Pits111 <isabel.mehlmann@gmail.com>
2021-07-16 12:31:40 +0200

    changing conf file password

--- db/conf.js
+++ db/conf.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "wilhelmine24",
+  password: "Isa304380!",
   database: "registration",
 });
 module.exports = connection;

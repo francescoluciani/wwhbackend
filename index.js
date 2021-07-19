5f2408003b8e93c8cc73a72ccd0187499036c79c
Pits111 <isabel.mehlmann@gmail.com>
2021-07-19 13:57:28 +0200

    changing node engine version in package.json

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/users", (req, res) => {
 });
 
 app.listen(port, (err) => {
-  if (err) throw new Error("ups something is not working");
+  if (err) throw new Error(" something is not working");
   console.log(`Great, your server is running on port: ${port}`);
 });
 

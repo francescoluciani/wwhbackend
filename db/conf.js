c63879d2e8ad742b3b9b426b6c5a68a0e2487f08
Pits111 <isabel.mehlmann@gmail.com>
2021-07-23 14:54:17 +0200

    added online database info

--- db/conf.js
+++ db/conf.js
@@ -11,4 +11,8 @@ const connection = mysql.createConnection({
 module.exports = connection;
 
 //mysql://b5773e640953d0:900d0f66@us-cdbr-east-04.cleardb.com/heroku_c89e4ed71ee48dc?reconnect=true
+// host: "b5773e640953d0",
+//   user: "us-cdbr-east-04.cleardb.com",
+//   password: "900d0f66",
+//   database: "heroku_c89e4ed71ee48dc",
 

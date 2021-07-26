ef637d3ba3f6641c7fe5815f22db41e6eb8d8e43
Pits111 <isabel.mehlmann@gmail.com>
2021-07-26 10:38:51 +0200

    deleting auth.js

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { loginValidation } = require("../routes/validation");
-const connection = require("../db/conf");
-const bcrypt = require("bcryptjs");
-
-module.exports = router;

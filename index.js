83e5fc72e67b71e5d902cc029941e61adae514f1
Pits111 <isabel.mehlmann@gmail.com>
2021-07-26 13:28:46 +0200

    adding db clear password to config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const loginroute = require("./routes/loginroute");
 const { userLogin } = require("./controller/userController");
 
@@ -15,17 +15,17 @@ const userModels = require("./models/userModels");
 app.get("/", (req, res) => res.send("Hello from /"));
 app.use("/auth", loginroute);
 
-// app.get("/users", (req, res) => {
-//   connection.query("SELECT * FROM user ", (err, results) => {
-//     if (err) {
-//       res.status(500).send("error retrieving data from db");
-//     } else {
-//       res.json(results);
-//     }
-//   });
-// });
+app.get("/users", (req, res) => {
+  connection.query("SELECT * FROM user ", (err, results) => {
+    if (err) {
+      res.status(500).send("error retrieving data from db");
+    } else {
+      res.json(results);
+    }
+  });
+});
 
-app.listen(port, (err) => {
+app.listen(PORT, (err) => {
   if (err) throw new Error(" something is not working");
-  console.log(`Great, your server is running on port: ${port}`);
+  console.log(`Great, your server is running on port: ${PORT}`);
 })

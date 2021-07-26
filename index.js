e8d8e93110f4f637c3fd1df65fbdb3ec469bc295
Pits111 <isabel.mehlmann@gmail.com>
2021-07-26 14:55:48 +0200

    adding app.use in index.js

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const { userLogin } = require("./controller/userController");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 const connection = require("./db/conf");
-const transactions = require("./db/data");
-const userModels = require("./models/userModels");
 
 app.get("/", (req, res) => res.send("Hello from /"));
 app.use("/auth", loginroute);

e669345e3cdde4b384ea76075f0b1979417528a4
Pits111 <isabel.mehlmann@gmail.com>
2021-07-27 10:18:59 +0200

    config file back to old db info

--- db/conf.js
+++ db/conf.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
 
   host: "us-cdbr-east-04.cleardb.com",
   user: "b5773e640953d0",
@@ -11,8 +11,4 @@ const connection = mysql.createPool({
 module.exports = connection;
 
 //mysql://b5773e640953d0:900d0f66@us-cdbr-east-04.cleardb.com/heroku_c89e4ed71ee48dc?reconnect=true
-//   hostname: "us-cdbr-east-04.cleardb.com",
-//   user: "b5773e640953d0"
-//   password: "900d0f66",
-//   database: "heroku_c89e4ed71ee48dc",
 
